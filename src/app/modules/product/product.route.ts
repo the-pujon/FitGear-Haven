e2496d75fe4bf79458bacc36dda4d12455d4c348
Pujon Das Auvi <auvipujondas@gmail.com>
2024-10-06 21:22:19 +0600

    new product api route added

--- src/app/modules/product/product.route.ts
+++ src/app/modules/product/product.route.ts
@@ -2,11 +2,13 @@ import { Router } from "express";
 import { ProductController } from "./product.controller";
 import { ProductValidation } from "./product.validation";
 import validateRequest from "../../middlewares/validateRequess";
+import { authorization } from "../../middlewares/authorization";
 
 const router = Router();
 
 router.post(
   "/create",
+  authorization("admin"),
   validateRequest(ProductValidation.createProductZodSchema),
   ProductController.createProduct,
 );
@@ -16,10 +18,11 @@ router.get("/:id", ProductController.getSingleProduct);
 
 router.patch(
   "/:id",
+  authorization("admin"),
   validateRequest(ProductValidation.updateProductZodSchema),
   ProductController.updateProduct,
 );
 
-router.delete("/:id", ProductController.deleteProduct);
+router.delete("/:id", authorization("admin"), ProductController.deleteProduct);
 
 export const ProductRoutes = router;
